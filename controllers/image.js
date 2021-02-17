4706559b04b26693f002dcfbdc43c8601cb1fad4
Eros Casas <ecasas182015@gmail.com>
2021-02-17 13:54:42 -0600

    hidden API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: "417a4b450e3042dc90979d41d363adc0"
+    apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
